56159e119c85a7373700f058e4a8e800c716deff
Mario Theodor <dragutmariotheodor1@gmail.com>
2023-06-06 13:13:56 +0300

    Updated package version

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,12 @@
-export { default as Show } from "./Show/Show";
-export { default as For } from "./For/For";
+/* Switch Group */
 export { default as Switch } from "./SwitchGroup/Switch/Switch";
 export { default as Case } from "./SwitchGroup/Case/Case";
+
+/* For */
+export { default as For } from "./For/For";
+
+/* Show */
+export { default as Show } from "./Show/Show";
+
+/* ScrollTo */
 export { default as ScrollTo } from "./ScrollTo/ScrollTo";
